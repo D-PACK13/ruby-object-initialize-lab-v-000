d14388bd4b23161eda27dd6f825f9bfdf0cae350
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-26 22:52:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,12 +21,6 @@ class Dog
     @breed = breed
   end
   def name
-    @name
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
+    "#{@name} #{@breed}".strip
   end
 end
