1c3f3e2f6b7105463e031fa2e9af7a5f3df0732c
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-04-26 22:46:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Person
-
+  def initialize(name)
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 class Dog
-  def initialize (name)
+  def initialize(name)
     @name = name
   end
   def name=(name)
